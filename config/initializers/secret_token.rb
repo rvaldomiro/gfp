4d3b30540734e60ffd2ae1c9b9b4c500cff9da94
Roberto Valdomiro <roberto@logus-consultoria.com.br>
2012-05-17 08:14:49 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gfp::Application.config.secret_token = '3527816f0cc340ed232da1b98bc88044f49f5a2c1b022e68ada36ee6358b1dd62688ace850535c45c8fe4d0162b844ef8ca43a48b6a86c89b5de2c8dd63da27f'
